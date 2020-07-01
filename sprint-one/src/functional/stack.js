f037a5b949015cfa566683aa16040262724db214
Ryan Zigler <rpzigler@gmail.com>
2020-07-01 18:37:35 -0500

    Implemented functional/stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,69 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var lowestKey = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    // add string to top of stack
+    storage[lowestKey] = value;
+    lowestKey -= 1;
   };
 
   someInstance.pop = function() {
+    // remove and return string from top of stack
+    lowestKey += 1;
+    let result = storage[lowestKey];
+    delete storage[lowestKey];
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
+/*
+-1: b
+0: a
+
+
+
+
+*/
+
+
+
+/*
+  var highestKey = 0;
+  var lowestKey = 0;
+
+  // Implement the methods below
+
+  someInstance.enqueue = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[highestKey] = value;
+      highestKey += 1;
+    } else {
+      storage[highestKey] = value;
+      highestKey += 1;
+    }
+  };
+
+  someInstance.dequeue = function() {
+    let result = storage[lowestKey];
+    delete storage[lowestKey];
+    lowestKey += 1;
+    return result;
+  };
+
+  someInstance.size = function() {
+    return Object.keys(storage).length;
+  };
+
+  return someInstance;
+};
+
+*/
