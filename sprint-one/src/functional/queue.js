09bde9f6d6d72d792be53a979bcf675db9dd2c7e
Ryan Zigler <rpzigler@gmail.com>
2020-07-01 18:00:35 -0500

    Implemented functional/queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,44 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  /* {0: a.
+      1: b,
+      2: c
+    }
+
+ */
+  // global variable for higest current key value
+  var highestKey = 0;
+  var lowestKey = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[highestKey] = value;
+      highestKey += 1;
+    } else {
+      storage[highestKey] = value;
+      highestKey += 1;
+    }
   };
 
   someInstance.dequeue = function() {
+    let result = storage[lowestKey];
+    delete storage[lowestKey];
+    lowestKey += 1;
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
+// queue.enqueue('a');
+// queue.enqueue('b');
+// queue.dequeue();
+// expect(queue.size()).to.equal(1);
