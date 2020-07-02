7ce59d4cdc77b0ba86e9f8101e5634668182e45c
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-07-02 16:07:35 -0700

    Completes the stack.js function in prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.lowestKey = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.lowestKey] = value;
+    this.lowestKey -= 1;
+  },
+  pop: function() {
+    this.lowestKey += 1;
+    let result = this.storage[this.lowestKey];
+    delete this.storage[this.lowestKey];
+    return result;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
