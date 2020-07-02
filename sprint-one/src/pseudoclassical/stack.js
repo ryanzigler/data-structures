91d428afec89ec831b37bf849e24fdd53c10a47c
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-07-02 16:31:18 -0700

    Completes the stack() in pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.lowestKey = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.lowestKey] = value;
+  this.lowestKey -= 1;
+};
 
+Stack.prototype.pop = function() {
+  this.lowestKey += 1;
+  let result = this.storage[this.lowestKey];
+  delete this.storage[this.lowestKey];
+  return result;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var someInstance = new Stack();
