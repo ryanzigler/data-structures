e160111b9de5badc2d94143067ffb9eab27d7cf2
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-07-02 16:37:34 -0700

    Completes stack() in es6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.lowestKey = 0;
   }
 
+  push(value) {
+    this.storage[this.lowestKey] = value;
+    this.lowestKey -= 1;
+  }
+
+  pop() {
+    this.lowestKey += 1;
+    let result = this.storage[this.lowestKey];
+    delete this.storage[this.lowestKey];
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var someInstance = new Stack();
