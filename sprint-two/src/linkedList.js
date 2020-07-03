650aa3a8c965ef80d36cf1afa956777bc7735183
Ryan Zigler <rpzigler@gmail.com>
2020-07-03 15:15:28 -0500

    Completes the linked list function inside linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,43 @@
+//
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
+  // time complexity: constant | O(1)
   list.addToTail = function(value) {
+    let node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
+  // time complexity: constant | O(1)
   list.removeHead = function() {
+    let headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
   };
 
+  // time complexity: linear | O(n)
   list.contains = function(target) {
+    let node = list.head;
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
-
   return list;
 };
 
+// time complexity: constant | O(1)
 var Node = function(value) {
   var node = {};
 
