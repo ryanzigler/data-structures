2d2c94dfb9e4e1260f7235bbb7c5c298e316b4a6
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-07-06 16:16:58 -0700

    Completes Set() with time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-};
+  this._storage.item = 1;
+};  // Time complexity: Constant O(1)
 
 setPrototype.contains = function(item) {
-};
+  if (this._storage.item) {
+    return true;
+  }
+  return false;
+}; // Time complexity: Constant O(1)
 
 setPrototype.remove = function(item) {
-};
+  delete this._storage.item;
+}; // Time complexity: Constant O(1)
 
 /*
  * Complexity: What is the time complexity of the above functions?
