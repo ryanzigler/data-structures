969729d794ed4937735bb1eea32de46de4f38319
Ryan Zigler <rpzigler@gmail.com>
2020-07-07 17:53:24 -0500

    setSpec.js now tests whether sets contain duplicate values.

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -24,4 +24,13 @@ describe('set', function() {
     expect(set.contains('Mel Gibson')).to.equal(false);
   });
 
+  it('should not contain duplicate values', function() {
+    set.add('Mel Gibson');
+    set.add('Mel Gibson');
+    set.remove('Mel Gibson');
+    expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+
+
 });
