51738b9d2b5efe79218ecd8d7783859da2e9aab1
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-07-06 20:56:43 -0700

    Begins outline work on binarysearchTree();

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,57 @@
 var BinarySearchTree = function(value) {
+
+  let treeNode = Object.create(nodeMethods); // {}
+
+  treeNode.value = value;
+  treeNode.left = null; // less than
+  treeNode.right = null; // greater than
+
+  return treeNode;
+};
+
+var nodeMethods = {
+  insert: function(value) {
+    // create new node
+    // var newNode = BinarySearchTree(value);
+    var insideFunction = function(currentNode, value) {
+
+      if (currentNode.value === undefined) {
+        BinarySearchTree(value);
+
+      } else if (value > currentNode.value) {
+        insideFunction(currentNode.right, value);
+      } else {
+        insideFunction(currentNode.left);
+      }
+
+      insideFunction(currentNode);
+
+      if (value < currentNode.value && currentNode.value === undefined) {
+        currentNode.left = BinarySearchTree(value);
+      }
+    };
+
+  },
+
+
+  // .contains()
+  contains: function(target) {
+    let result = false;
+    const findTarget = function(node) {
+      if (node.value === target) {
+        result = true;
+        return result;
+      }
+      _.each(node.children, (child) => findTarget(child));
+    };
+    findTarget(this);
+    return result;
+  },
+
+  // .depthFirstLog()
+  depthFirstLog: function() {
+
+  }
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,10 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
   if (this._storage[index] === undefined) {
     var bucket = [];
     bucket.push([k, v]);
     this._storage[index] = bucket;
-    // bucket.push([k, v]);
   } else {
     for (var i = 0; i < this._storage[index].length; i++) {
       if (this._storage[index][i][0] === k) {
@@ -20,11 +18,7 @@ HashTable.prototype.insert = function(k, v) {
       }
     }
   }
-};
-
-// |-storage  |- bucket |- tuples
-// [          [         [k,v], [k,v], [k,v] ], [], [] ]
-//sto = [[k, v], [k,v], ]
+}; // Time complexity: Linear O(n)
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -34,25 +28,21 @@ HashTable.prototype.retrieve = function(k) {
       return bucket[i][1];
     }
   }
-};
+}; // Time complexity: Linear O(n)
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
   let bucket = this._storage[index];
   for (let i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       bucket.splice(i, 1);
     }
   }
-};
+}; // Time complexity: Linear O(n)
 
-// this.map.get(fromNode).splice(this.map.get(fromNode).indexOf(toNode), 1);
-
+// |-storage  |- bucket |- tuples
+// [          [         [k,v], [k,v], [k,v] ], [], [] ]
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-//  [ Index[0]: {Value: Hash},  Index[1]: {Value: Hash} ]
-// [ {Steven: H1234}, {Tyler: H03871} ]
